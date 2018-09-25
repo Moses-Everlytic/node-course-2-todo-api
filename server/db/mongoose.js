b8c5a12cd35ed2932aae4e37a73cdcb1c1b85fa4
Mozi-k <moses@everlytic.com>
2018-09-25 13:11:09 +0200

    Setup seperate test and developemnt env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp'); //'mongodb://udemyheroku:Herokuconnect1@ds127894.mlab.com:27894/udemyheroku'
+mongoose.connect(process.env.MONGODB_URI); //'mongodb://udemyheroku:Herokuconnect1@ds127894.mlab.com:27894/udemyheroku'
 
 module.exports = {
   mongoose
